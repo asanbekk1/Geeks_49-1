bdf3a6021303963347591834f59fc2b19c993842
aidarjeenbekov <aidarjeenbekov444@gmail.com>
2025-02-26 20:50:26 +0600

    manage.py

--- parser_app/forms.py
+++ parser_app/forms.py
@@ -3,7 +3,7 @@ from . import models, parser_rezka
 
 class ParserForm(forms.Form):
     MEDIA_CHOICES = (
-        ('rezka.ag', 'rezka.ag'),
+        ('mybook.ru', 'mybook.ru'),
     )
     media_type = forms.ChoiceField(choices=MEDIA_CHOICES)
 
@@ -12,7 +12,7 @@ class ParserForm(forms.Form):
             'media_type',
         ]
     def parser_data(self):
-        if self.data['media_type'] == 'rezka.ag':
+        if self.data['media_type'] == 'mybook.ru':
             rezka_films = parser_rezka.parsing_rezka()
             for i in rezka_films:
                 models.RezkaModel.objects.create(**i)
